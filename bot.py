19794b9c7c8698faaa055762d949ce97d1669fbf
Alvaro Picatoste <apicatoste@gmail.com>
2022-02-02 09:20:38 +0100

    Working Bot deployed in Heroku

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import logging
 import telegram
 import random
+import sys
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Configurar Logging
@@ -14,7 +15,33 @@ logger = logging.getLogger()
 # Solicitar TOKEN
 
 TOKEN = os.getenv("TOKEN_TELEGRAM")
+mode = os.getenv('RUN_MODE')
 
+if mode == "dev":
+    # Acceso Local (Desarrollo)
+    def run(updater):
+        updater.start_polling()
+        print("BOT CARGADO")
+        updater.idle() # Permite finalizar el bot con Ctrl + C
+        
+elif mode == "prod":
+    # Acceso Servidor (producción):
+    def run(updater):
+        PORT = int(os.environ.get('PORT', '8443')) # Telegram API Supported ports for Webhook: 443, 80, 88, 8443
+        print(PORT)
+        APP_NAME = os.environ.get("APP_NAME")
+        # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
+
+        updater.start_webhook(listen="0.0.0.0",
+                            port=PORT,
+                            url_path=TOKEN,
+                            webhook_url=f"https://{APP_NAME}.herokuapp.com/{TOKEN}")
+
+else:
+    logger.info("No se especifico el modo")
+    sys.exit()
+    
+    
 def start(update, context):
     logger.info(f"El usuario {update.message.chat['username']} ha iniciado una conversacion")
     name = update.message.chat['username']
@@ -28,7 +55,7 @@ def random_number(update, context):
     
 def echo(update, context):
     user_id = update.message.chat['id']
-    logger.info(f"El usuario {user_id} ha  envioado un mensaje")
+    logger.info(f"El usuario {user_id} ha enviado un mensaje")
     text = update.message.text
     context.bot.sendMessage(chat_id=user_id, parse_mode="MarkdownV2", text=f"*Mensaje replicado:*\n_{text}_")
 
@@ -49,6 +76,4 @@ dp.add_handler(CommandHandler("start", start))
 dp.add_handler(CommandHandler("random", random_number))
 dp.add_handler(MessageHandler(Filters.text, echo))
 
-updater.start_polling()
-print('BOT CARGADO')
-updater.idle() # Permite finalizar el bot con ctrl+E
+run(updater)
